c3a72a6f85caebb728c6b60c5ef705600aab7c23
Dilek <dilekervc@gmail.com>
2025-11-07 17:42:26 +0300

    Initial Commit

---
+++ TariflikDemo/TariflikDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TariflikDemoApp.swift
+//  TariflikDemo
+//
+//  Created by Dilek Eravcı on 7.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct TariflikDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
